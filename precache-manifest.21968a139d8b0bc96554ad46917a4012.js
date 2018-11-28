1290dca17f1dc27d7beaf20f5286abb209b81919
Marcin Roszczyk <marcin.roszczyk@tutara.pl>
2018-11-28 12:31:45 +0100

    Updates

---
+++ precache-manifest.21968a139d8b0bc96554ad46917a4012.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/cv_app/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "48747558a5369daad875",
+    "url": "/cv_app/static/js/runtime~main.48747558.js"
+  },
+  {
+    "revision": "1d820e0c0ab528e64fcb",
+    "url": "/cv_app/static/js/main.1d820e0c.chunk.js"
+  },
+  {
+    "revision": "5464eddd297ea618a473",
+    "url": "/cv_app/static/js/1.5464eddd.chunk.js"
+  },
+  {
+    "revision": "1d820e0c0ab528e64fcb",
+    "url": "/cv_app/static/css/main.01e30539.chunk.css"
+  },
+  {
+    "revision": "b46359e73250415a0e280b67b2d14d21",
+    "url": "/cv_app/index.html"
+  }
+];
